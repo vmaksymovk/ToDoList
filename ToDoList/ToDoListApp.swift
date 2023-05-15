fadc9a2800663b06a54a003fa252f519fd75170b
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-05-15 10:37:27 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Влад on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
