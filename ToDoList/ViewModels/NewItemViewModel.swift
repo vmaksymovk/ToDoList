a58955e844e60dfe56f7d6261f9aed56a9a8b161
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-05-17 20:40:33 +0200

    created and modify save() func. Connected with firebase collection

--- ToDoList/ViewModels/NewItemViewModel.swift
+++ ToDoList/ViewModels/NewItemViewModel.swift
@@ -4,18 +4,40 @@
 //
 //  Created by Влад on 5/15/23.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel : ObservableObject{
     @Published var title = ""
     @Published var dueDate = Date()
     @Published var showAlert = false
+    
     init() {
         
     }
     
     func save(){
+        guard canSave else{
+            return
+        }
+        // get current user id
+        guard let uID = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        
+        // create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId, title: title, dueDate: dueDate.timeIntervalSince1970, createdDate: Date().timeIntervalSince1970, isDone: false)
+        //save model
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uID)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
         
     }
     
